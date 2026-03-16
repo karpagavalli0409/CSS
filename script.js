b5e9046a5e396aaf46b50704317eef2035248b01
karpagavalli0409 <kavisaro98@gmail.com>
2026-03-16 11:58:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1.Declare Constant
+const COMPANYNAME="Innovate Corp";
+console.log(COMPANYNAME);
+//2. Declare Dynamic
+let projectCount= 5;
+console.log(projectCount);
+//3. Check Type
+console.log(typeof projectCount);
+//4. Reassign
+projectCount= 6;
+console.log(projectCount);
+//5. Declare Boolean
+const hasPermission= true;
+console.log(hasPermission);
+//6. Undefined Variable
+let futureFeature;
+console.log(futureFeature, typeof futureFeature);
+//7. Array Declaration
+const WEEKDAYS= ["Mon", "Tue", "Wed"];
+console.log(WEEKDAYS);
+//8. Access Array Item
+console.log(WEEKDAYS[0]);
+//9. Object Declaration
+const widget= {
+    color: "blue",
+    price: 99.99
+};
+console.log(widget) 
+//10.Template Literal Output
+console.log(' At ${COMPANYNAME}, we are managing ${projectCount} projects. ') 
